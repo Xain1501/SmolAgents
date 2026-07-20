75a039f0cd963ca03ab66a67af43c44fc447b64f
xain1501 <xain.saqib@gmail.com>
2026-07-20 23:24:45 +0500

    updated requirements.txt

---
+++ module2/2.1/DynamicVisionAgent.py
@@ -0,0 +1,125 @@
+import os
+import PIL
+from dotenv import load_dotenv
+from time import sleep
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+from smolagents import CodeAgent,tool,InferenceClientModel, DuckDuckGoSearchTool, ActionStep
+
+import helium
+from helium import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from helium import start_chrome, get_driver
+
+start_chrome("https://google.com")
+
+driver = get_driver()
+
+# Load environment variables from .env file
+load_dotenv(os.path.join(os.path.dirname(__file__), '../../.env'))
+
+# Get HuggingFace token from environment
+hf_token = os.getenv('HF_TOKEN')
+
+model = InferenceClientModel(
+    model_id="Qwen/Qwen2.5-VL-72B-Instruct",
+    token=hf_token,
+)
+
+
+image_urls = [
+    "https://upload.wikimedia.org/wikipedia/commons/e/e8/The_Joker_at_Wax_Museum_Plus.jpg", # Joker image
+    "https://upload.wikimedia.org/wikipedia/en/9/98/Joker_%28DC_Comics_character%29.jpg" # Joker image
+]
+
+images = []
+for url in image_urls:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36" 
+    }
+    response = requests.get(url,headers=headers)
+    image = PIL.Image.open(BytesIO(response.content)).convert("RGB")
+    images.append(image)
+
+
+
+
+
+@tool
+def search_item_ctrl_f(text :str, nth_result:int =1)->str:
+
+  """
+  Searches for text on the current page via Ctrl + F and jumps to the nth occurrence.
+    Args:
+        text: The text to search for
+        nth_result: Which occurrence to jump to (default: 1)
+  
+  """
+  elements = driver.find_elements(By.XPATH, f"//*[contains(text(), '{text}')]")
+  if nth_result > len(elements):
+        raise Exception(f"Match n°{nth_result} not found (only {len(elements)} matches found)")
+  result = f"Found {len(elements)} matches for '{text}'."
+  elem = elements[nth_result - 1]
+  driver.execute_script("arguments[0].scrollIntoView(true);", elem)
+  result += f"Focused on element {nth_result} of {len(elements)}"
+  return result
+
+
+@tool
+def go_back() -> None:
+    """Goes back to previous page."""
+    driver.back()
+
+@tool
+def close_popups() -> str:
+    """
+    Closes any visible modal or pop-up on the page. Use this to dismiss pop-up windows! This does not work on cookie consent banners.
+    """
+    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+def save_screenshot(step_log: ActionStep, agent: CodeAgent) -> None:
+    sleep(1.0)  # Let JavaScript animations happen before taking the screenshot
+    driver = helium.get_driver()
+    current_step = step_log.step_number
+    if driver is not None:
+        for step_logs in agent.logs:  # Remove previous screenshots from logs for lean processing
+            if isinstance(step_log, ActionStep) and step_log.step_number <= current_step - 2:
+                step_logs.observations_images = None
+        png_bytes = driver.get_screenshot_as_png()
+        image = Image.open(BytesIO(png_bytes))
+        print(f"Captured a browser screenshot: {image.size} pixels")
+        step_log.observations_images = [image.copy()]  # Create a copy to ensure it persists, important!
+
+    # Update observations with current URL
+    url_info = f"Current url: {driver.current_url}"
+    step_log.observations = url_info if step_logs.observations is None else step_log.observations + "\n" + url_info
+    return
+
+
+agent = CodeAgent(
+    tools=[DuckDuckGoSearchTool(), go_back, close_popups, search_item_ctrl_f],
+    model=model,
+    additional_authorized_imports=["helium"],
+    step_callbacks=[save_screenshot],
+    max_steps=20,
+    verbosity_level=2,
+)
+
+
+response = agent.run(
+    """
+    You MUST use DuckDuckGoSearchTool and other tools provided to you.
+    Describe the costume and makeup that the comic character in these photos is wearing and return the description.
+    Tell me if the guest is The Joker or Wonder Woman.
+
+    Do not answer from memory.
+    """,
+    images=images
+    
+)
+
